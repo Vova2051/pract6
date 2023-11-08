dc647ca4dc82e3f24ac1488f37aa46890bccbf38
Vova2051 <volodavinogradskij@gmail.com>
2023-11-08 14:01:50 +0200

    add !!!

--- git.testoviy/src/ava/Kalta.java
+++ git.testoviy/src/ava/Kalta.java
@@ -3,7 +3,7 @@ package ava;
 public class Kalta {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from GITHUBBBBB");
+		System.out.println("Hello!!! from GITHUBBBBB");
 
 	}
 
